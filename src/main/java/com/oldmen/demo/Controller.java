196cd86c9a6d4516e9ab4353a1e1778bdc184975
Suebsakul Ampa <kul1977@gmail.com>
2023-01-28 13:08:55 +0700

    Update code to EP2

--- src/main/java/com/oldmen/demo/Controller.java
+++ src/main/java/com/oldmen/demo/Controller.java
@@ -4,20 +4,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.java.Log;
+
+@Log
 @RestController
 public class Controller {
 	
-	List<employee> employees = new ArrayList<>(
+	List<employee> employees = new ArrayList<employee>(
 			Arrays.asList(
 						new employee(1001, "John", "4340 Virgil Street Port St Joe FL Flori"    ,"850-227-8539", 32456),
 						new employee(1002, "Bob" , "1885 Fairfield Road Milwaukee WI Wisconsin" ,"262-586-3369", 53213)						
 					));
 	
-	@RequestMapping("/employee")
+	@GetMapping("/employee")
 	public List<employee> getAllEmployee() {
 	    return employees;
 	}
+	
+	@PostMapping("/employee")
+	public boolean addEmployee(@RequestBody employee Employee) {
+		
+		log.info("Employee recieve : " + Employee.toString());
+		
+	    return true;
+	}	
 }
