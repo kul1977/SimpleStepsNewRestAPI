7e10c727ff679d2e782a0f3ab3395bad997b0e35
Suebsakul Ampa <kul1977@gmail.com>
2023-02-08 18:58:42 +0700

    add 2 components (swagger, actuator)

--- src/main/java/com/oldmen/demo/Controller.java
+++ src/main/java/com/oldmen/demo/Controller.java
@@ -4,14 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.java.Log;
 
 @Log
+@CrossOrigin
 @RestController
 public class Controller {
 	
@@ -21,16 +29,26 @@ public class Controller {
 						new employee(1002, "Bob" , "1885 Fairfield Road Milwaukee WI Wisconsin" ,"262-586-3369", 53213)						
 					));
 	
+	@Operation(summary = "Get a employee")
+	@ApiResponses(value = { 
+	  @ApiResponse(responseCode = "200", description = "Found the employee", 
+	    content = { @Content(mediaType = "application/json", 
+	      schema = @Schema(implementation = employee.class)) }),
+	  @ApiResponse(responseCode = "400", description = "Invalid id Employee", 
+	    content = @Content), 
+	  @ApiResponse(responseCode = "404", description = "Employee not found", 
+	    content = @Content) })
 	@GetMapping("/employee")
 	public List<employee> getAllEmployee() {
 	    return employees;
 	}
-	
+			
 	@PostMapping("/employee")
 	public boolean addEmployee(@RequestBody employee Employee) {
 		
 		log.info("Employee recieve : " + Employee.toString());
 		
+		employees.add(Employee);
 	    return true;
 	}	
 }
--- src/main/java/com/oldmen/demo/employee.java
+++ src/main/java/com/oldmen/demo/employee.java
@@ -2,9 +2,11 @@ package com.oldmen.demo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class employee {
 	private int id;
 	private String name;
